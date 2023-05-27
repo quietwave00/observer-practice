291997997ac053cd2f776c3176c8ec8e42cfa38b
갸갸 <quietwave00@gmail.com>
2023-05-27 22:35:30 +0900

    delete

--- src/main/java/com/observer/action/Subject.java
+++
@@ -1,8 +0,0 @@
-package com.observer.action;
-
-//observer들이 상태값을 받을 객체
-public interface Subject {
-    void subscribe(Observer observer);
-    void unSubscribe(Observer observer);
-    void notifyToObserver(String msg);
-}
