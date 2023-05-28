45b75ff056b02341e7fd866159d965b02fb1c307
갸갸 <quietwave00@gmail.com>
2023-05-28 23:11:15 +0900

    subject client에서 EventSource로 요청

--- client/subject.js
+++ client/subject.js
@@ -1,21 +1,22 @@
 document.getElementById("subscribe-button").addEventListener('click', function() {
     for(let i = 1; i <= 3; i++) {
-        console.log("subscribe run");
-        fetch("http://localhost:8080/subscribe/" + i, {
-            method: "GET",
-            mode: "cors"
-            })
-        .then(console.log('complete'))
+        const eventSource = new EventSource('http://localhost:8080/subscribe/' + i);
+
+        eventSource.addEventListener('message', (e) => {
+            const message = e.data;
+            console.log("message: "  + message);
+        })
     }
 });
     
 document.getElementById("upload-button").addEventListener("click", function() {
     fetch('http://localhost:8080/upload', {
-        method: "GET",
-        mode: "cors"
+        method: "GET"
     })
-    .then((response) => response.json())
     .then((response) => {
-        console.log(response);
+        return response.text();
+    })
+    .then((data) => {
+        const msg = data;
     })
 });
