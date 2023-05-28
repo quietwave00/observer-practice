bc593a6d46ca2e963ecffdc5b04a3fc9970febb4
갸갸 <quietwave00@gmail.com>
2023-05-28 23:11:55 +0900

    Action Interface 수정

---
+++ src/main/java/com/observer/action/AlertAction.java
@@ -0,0 +1,5 @@
+package com.observer.action;
+
+public interface AlertAction {
+    void add(String data);
+}
--- src/main/java/com/observer/action/SubjectAction.java
+++ src/main/java/com/observer/action/SubjectAction.java
@@ -1,10 +1,14 @@
 package com.observer.action;
 
 import com.observer.entity.Observer;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
 
 //observer들이 상태값을 받을 객체
 public interface SubjectAction {
     void subscribe(Observer observer);
     void unSubscribe(Observer observer);
-    void notifyToObserver(String msg);
+    void notifyToObserver(String msg) throws IOException;
+
 }
