55d128d63ee16b485b69fc08e5b58cc98475eac3
갸갸 <quietwave00@gmail.com>
2023-06-06 23:13:12 +0900

    Subject에서 503에러 방지를 위한 dummy data 수신하기

--- src/main/java/com/observer/main/subject/SubjectService.java
+++ src/main/java/com/observer/main/subject/SubjectService.java
@@ -17,25 +17,39 @@ public class SubjectService {
     private final ObserverRepository observerRepository;
     private final SubjectRepository subjectRepository;
 
-    public SseEmitter subscribe(Long observerId) throws IOException {
+    public SseEmitter subscribe(Long observerId, String lastEventId) throws IOException {
         Subject beforeSubject = subjectRepository.findBySubjectId(1L);
         Observer beforeObserver = observerRepository.findByObserverId(observerId);
         beforeObserver.addSubject(beforeSubject);
         Observer afterObserver = observerRepository.save(beforeObserver);
         beforeSubject.subscribe(afterObserver);
         Subject afterSubject = subjectRepository.save(beforeSubject);
-        System.out.println("dd: " + afterSubject.getSubjectId());
-        
 
         SseEmitter emitter = new SseEmitter();
         emitter.send(SseEmitter.event().name("init"));
-//        emitter.onCompletion(() -> afterSubject.getObserverList().remove(findObserver));
         emitter.onCompletion(() -> EmitterStorage.removeEmitter(afterObserver.observerId));
+        emitter.onTimeout(() -> EmitterStorage.removeEmitter(afterObserver.observerId));
+
+        String emitterId = observerId + "_" + System.currentTimeMillis();
+        //503 에러 방지
+        sendToObserver(emitter, emitterId, observerId);
 
         EmitterStorage.addEmitter(observerId, emitter);
         EmitterStorage.addObserverId(emitter, observerId);
 
         return emitter;
+    }
+
+    private void sendToObserver(SseEmitter emitter, String emitterId, Long observerId) {
+        try {
+            emitter.send(SseEmitter.event()
+                    .id(emitterId)
+                    .name("sse")
+                    .data("EventStream Created"));
+        } catch(IOException e) {
+            EmitterStorage.removeEmitter(observerId);
+            throw new RuntimeException("Connection Error");
+        }
     }
 
     public void upload(){
