9b7bd18852b33f892a62073a147d703326c6ec0f
갸갸 <quietwave00@gmail.com>
2023-06-12 23:49:29 +0900

    CompletableFutre를 이용한 순서 보장?

--- src/main/java/com/observer/main/subject/SubjectController.java
+++ src/main/java/com/observer/main/subject/SubjectController.java
@@ -1,14 +1,15 @@
 package com.observer.main.subject;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 
 @RestController
@@ -18,9 +19,17 @@ public class SubjectController {
     private final SubjectService service;
 
 
-    @GetMapping(value = "/subscribe/{observerId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@PathVariable("observerId") Long observerId) throws IOException {
+    @GetMapping(value = "/subscribe/{observerId}", produces = "text/event-stream")
+    public CompletableFuture<SseEmitter> subscribe(@PathVariable("observerId") Long observerId,
+                                                   @RequestHeader(value = "Last-Event_Id", required = false, defaultValue = "") String lastEventId) throws IOException {
+        System.out.println("subject 알림 활성화 요청 들어옴");
         return service.subscribe(observerId);
+    }
+
+    @GetMapping(value = "/alerts/{observerId}", produces = "text/event-stream")
+    public CompletableFuture<SseEmitter> alert(@PathVariable("observerId") Long observerId) throws InterruptedException, ExecutionException {
+        System.out.println("observer 요청 들어옴");
+        return service.alert(observerId);
     }
 
     @GetMapping("/upload")
--- src/main/java/com/observer/main/subject/SubjectService.java
+++ src/main/java/com/observer/main/subject/SubjectService.java
@@ -2,18 +2,17 @@ package com.observer.main.subject;
 
 import com.observer.entity.Observer;
 import com.observer.entity.Subject;
+import com.observer.main.EmitterDto;
 import com.observer.main.observer.ObserverRepository;
 import com.observer.main.sse.EmitterStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionalEventListener;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -23,53 +22,54 @@ public class SubjectService {
 
     private final ObserverRepository observerRepository;
     private final SubjectRepository subjectRepository;
+    private final CompletableFuture<SseEmitter> future = new CompletableFuture<>();
+    public CompletableFuture<SseEmitter> subscribe(Long observerId) throws IOException {
+        dataSetting(observerId);
+        SseEmitter emitter = new SseEmitter();
+        EmitterDto dto = new EmitterDto(emitter, observerId);
+        emitter.send(SseEmitter.event().name("init").data("EventStream Created"));
 
-    @TransactionalEventListener
-    @Async
-    public SseEmitter subscribe(Long observerId) throws IOException {
-        System.out.println("실행체크");
+        EmitterStorage.addEmitter(observerId, dto);
 
-        try {
-            SseEmitter emitter =EmitterStorage.getEmitterByObserverId(observerId);
+        emitter.onCompletion(() -> {
+            System.out.println("emitter.onCompletion() Run");
+            EmitterStorage.removeEmitter(observerId);
+        });
+        emitter.onTimeout(() -> {
+            System.out.println("emitter.onTimeout() Run");
+            EmitterStorage.removeEmitter(observerId);
+        });
 
-            emitter.onCompletion(() -> {
-                System.out.println("emitter.onCompletion() Run");
-                EmitterStorage.removeEmitter(emitter);
-                EmitterStorage.removeEmitter(observerId);
-            });
-            emitter.onTimeout(() -> {
+        future.complete(emitter); // CompletableFuture에 결과 값을 설정
 
-                System.out.println("emitter.ontimeout() Run");
-                EmitterStorage.removeEmitter(emitter);
-                EmitterStorage.removeEmitter(observerId);
+        System.out.println("emitterSize :" + EmitterStorage.getMapSize());
+        System.out.println("observerSize : " + EmitterStorage.getMapSize());
 
-            });
-            return emitter;
+        return future;
+    }
 
-        } catch (NullPointerException e) {
-            System.out.println("처음요청");
-            dataSetting(observerId);
-            SseEmitter emitter = new SseEmitter();
-            emitter.send(SseEmitter.event().name("init"));
-            EmitterStorage.addEmitter(observerId, emitter);
-            EmitterStorage.addObserverId(emitter, observerId);
-            return emitter;
-        } finally {
-            System.out.println("emitterSize :" + EmitterStorage.getEmitterMapSize());
-            System.out.println("observerSize : " + EmitterStorage.getObserverMapSize());
-        }
-
-
+    public CompletableFuture<SseEmitter> alert(Long observerId) {
+        return future.thenApplyAsync(result -> {
+            SseEmitter emitter = EmitterStorage.getEmitterByObserverId(observerId);
+            if (emitter != null) {
+                return emitter;
+            } else {
+                try {
+                    return subscribe(observerId).join();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
     @Transactional
-    public Long dataSetting(final Long observerId) {
+    public void dataSetting(final Long observerId) {
         Subject beforeSubject = subjectRepository.findBySubjectId(1L);
         Observer beforeObserver = observerRepository.findByObserverId(observerId);
         beforeObserver.addSubject(beforeSubject);
         Observer afterObserver = observerRepository.save(beforeObserver);
         beforeSubject.subscribe(afterObserver);
         Subject afterSubject = subjectRepository.save(beforeSubject);
-        return afterSubject.getSubjectId();
     }
 
     private void sendToObserver(SseEmitter emitter, String emitterId, Long observerId) {
@@ -88,6 +88,7 @@ public class SubjectService {
         Subject findSubject = subjectRepository.findBySubjectId(1L);
         findSubject.upload();
     }
+
 
 
 }
